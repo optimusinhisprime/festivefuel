0e39c70d95efcac4bc2668f2f997a86bd1f5f036
scarface <pphiri1012x@gmail.com>
2022-05-02 00:40:41 +0200

    Add layout to control navbar and footer in one place

---
+++ components/Layout/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Flex, Box, Spacer } from '@chakra-ui/react'
+
+export default function Footer() {
+  return (
+    <Flex>
+      <Box>
+          Links 1
+      </Box>
+
+      <Box>
+          Links 2
+      </Box>
+
+      <Box>
+          Address and Map
+      </Box>
+    </Flex>
+  )
+}
---
+++ components/Layout/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Flex, Box, Spacer } from '@chakra-ui/react'
+
+
+export default function Menu({children}) {
+  return (
+    <Flex>
+        <Box>
+            FESTIVAL FUEL
+        </Box>
+
+        <Flex>Nav Links</Flex>
+      
+    </Flex>
+  )
+}
