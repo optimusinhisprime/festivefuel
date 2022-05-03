3db1f183f690682ae4ae526caa258972aa184839
scarface <pphiri1012x@gmail.com>
2022-05-03 13:44:12 +0200

    Add sample events file

---
+++ events.js
@@ -0,0 +1,32 @@
+export const eventList = [
+  {
+    name: "VIC FALLS CARNIVAL 2022",
+    logo: "https://scontent.fgbe1-1.fna.fbcdn.net/v/t39.30808-6/277583421_3047777872138699_350573559796029865_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=QqLqbEMOIUYAX_Jm852&_nc_oc=AQk4LzxrLG7hHpVXVCKqH_evaoPdD10LnC3GYljSY0PMFc_y4bJ9OobPZrwRBouzh84&tn=uTUaX_NEV9dGehbe&_nc_ht=scontent.fgbe1-1.fna&oh=00_AT_CwfcxqbjUGs_B3WLmbVdZz24APVal2SkCow3y6rTKOw&oe=6274B664",
+    date: "From 29 Apr 2022",
+  },
+  {
+    name: "KGATLENG FAMILY FUN DAY",
+    logo: "https://scontent.fgbe1-1.fna.fbcdn.net/v/t39.30808-6/277583421_3047777872138699_350573559796029865_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=QqLqbEMOIUYAX_Jm852&_nc_oc=AQk4LzxrLG7hHpVXVCKqH_evaoPdD10LnC3GYljSY0PMFc_y4bJ9OobPZrwRBouzh84&tn=uTUaX_NEV9dGehbe&_nc_ht=scontent.fgbe1-1.fna&oh=00_AT_CwfcxqbjUGs_B3WLmbVdZz24APVal2SkCow3y6rTKOw&oe=6274B664",
+    date: "30 Apr 2022",
+  },
+  {
+    name: "DARK KNIGHTS EVENTS PRESENTS",
+    logo: "https://scontent.fgbe1-1.fna.fbcdn.net/v/t39.30808-6/277583421_3047777872138699_350573559796029865_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=QqLqbEMOIUYAX_Jm852&_nc_oc=AQk4LzxrLG7hHpVXVCKqH_evaoPdD10LnC3GYljSY0PMFc_y4bJ9OobPZrwRBouzh84&tn=uTUaX_NEV9dGehbe&_nc_ht=scontent.fgbe1-1.fna&oh=00_AT_CwfcxqbjUGs_B3WLmbVdZz24APVal2SkCow3y6rTKOw&oe=6274B664",
+    date: "30 April 2022",
+  },
+  {
+    name: "MUNCHIES MARKET",
+    logo: "https://scontent.fgbe1-1.fna.fbcdn.net/v/t39.30808-6/277583421_3047777872138699_350573559796029865_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=QqLqbEMOIUYAX_Jm852&_nc_oc=AQk4LzxrLG7hHpVXVCKqH_evaoPdD10LnC3GYljSY0PMFc_y4bJ9OobPZrwRBouzh84&tn=uTUaX_NEV9dGehbe&_nc_ht=scontent.fgbe1-1.fna&oh=00_AT_CwfcxqbjUGs_B3WLmbVdZz24APVal2SkCow3y6rTKOw&oe=6274B664",
+    date: "30 Apr 2022",
+  },
+  {
+    name: "MGGENIX ENTERTAINMENT PRESENTS",
+    logo: "https://scontent.fgbe1-1.fna.fbcdn.net/v/t39.30808-6/277583421_3047777872138699_350573559796029865_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=QqLqbEMOIUYAX_Jm852&_nc_oc=AQk4LzxrLG7hHpVXVCKqH_evaoPdD10LnC3GYljSY0PMFc_y4bJ9OobPZrwRBouzh84&tn=uTUaX_NEV9dGehbe&_nc_ht=scontent.fgbe1-1.fna&oh=00_AT_CwfcxqbjUGs_B3WLmbVdZz24APVal2SkCow3y6rTKOw&oe=6274B664",
+    date: "30 Apr 2022",
+  },
+  {
+    name: "SEROWE WISE OUT CHILLAS",
+    logo: "https://scontent.fgbe1-1.fna.fbcdn.net/v/t39.30808-6/277583421_3047777872138699_350573559796029865_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=QqLqbEMOIUYAX_Jm852&_nc_oc=AQk4LzxrLG7hHpVXVCKqH_evaoPdD10LnC3GYljSY0PMFc_y4bJ9OobPZrwRBouzh84&tn=uTUaX_NEV9dGehbe&_nc_ht=scontent.fgbe1-1.fna&oh=00_AT_CwfcxqbjUGs_B3WLmbVdZz24APVal2SkCow3y6rTKOw&oe=6274B664",
+    date: "07 May 2022",
+  },
+];
