cbff5be39ff63ae937f2accfc83c9d86c2707162
scarface <pphiri1012x@gmail.com>
2022-05-03 13:45:10 +0200

    Add dropdown menu button

---
+++ components/Layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React,{ useEffect, useState } from 'react'
+import { Flex, Box, Link, Image, Button, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
+import { GrLogin, GrFormDown } from 'react-icons/gr';
+
+export default function Navbar({children}) {
+  return (
+    <Flex bg="#000E44" pr={7} pl={7}  align="center" justify="space-between">
+        <Box>
+            <Image w={200} src="/festivalfuel-logo.png"/>
+        </Box>
+
+        <Flex >
+        <Menu>
+  <MenuButton as={Button} rightIcon={<GrFormDown />}>
+    Profile
+  </MenuButton>
+  <MenuList>
+    
+    <MenuItem>Stall Requests</MenuItem>
+    <MenuItem>Shopping Cart</MenuItem>
+  <MenuItem>Account Settings</MenuItem>
+   <MenuItem>Logout</MenuItem>
+  </MenuList>
+</Menu>
+          <Button ml={3} leftIcon={<GrLogin/>} variant="solid">Sign In</Button>
+          </Flex>
+      
+    </Flex>
+  )
+}
