0744760a6b8c7567a5a958a2c7eb6c8295ee674d
scarface <pphiri1012x@gmail.com>
2022-05-04 22:03:41 +0200

    Add timestamps to stall payment model

--- models/StallPayment.js
+++ models/StallPayment.js
@@ -1,11 +1,16 @@
 import * as mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-const StallPaymentSchema = new Schema({
-  vendorId: String,
-  stallId: String,
-  amount: String,
-});
+const StallPaymentSchema = new Schema(
+  {
+    vendorId: String,
+    stallId: String,
+    amount: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports =
   mongoose.models.StallPayment ||
