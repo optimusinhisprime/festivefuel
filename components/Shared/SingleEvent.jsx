220fd695e5951c085d256bcdbf907df1d4a5b647
Les Chinyanga <les.chinyanga@africacode.academy>
2022-05-05 18:35:13 +0200

    Initial fetch and display of data for single event

--- components/Shared/SingleEvent.jsx
+++ components/Shared/SingleEvent.jsx
@@ -1,59 +1,68 @@
-import React from 'react'
-import { Flex, Image, Box, Heading, Button } from "@chakra-ui/react";
+import React from 'react';
+import { Flex, Image, Box, Heading, Button } from '@chakra-ui/react';
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableContainer,
-  } from '@chakra-ui/react'
-const SingleEvent = () => {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react';
+const SingleEvent = ({ event }) => {
+  const { date, time, expectedAttendance, description } = event;
   return (
-    <Flex mt={10}  align="center" direction="column"> 
-    <Flex direction={["column", "row"]}>
-        <Image w={400} src="https://umfworldwide.com/wp-content/uploads/2017/11/resistance-abu-dhabi-lineup.jpg"/>
+    <Flex mt={10} align='center' direction='column'>
+      <Flex direction={['column', 'row']}>
+        <Image
+          w={400}
+          src='https://umfworldwide.com/wp-content/uploads/2017/11/resistance-abu-dhabi-lineup.jpg'
+        />
         <Box>
-            <p>Date: </p>
-            <p>Time:</p>
-            <p>Anticipated Event Attendance:</p>
-            <p>Venue:</p>
-            <p>Description:</p>
+          <p>Date: {date}</p>
+          <p>Time: {time}</p>
+          <p>Anticipated Event Attendance: {expectedAttendance}</p>
+          <p>Venue:</p>
+          <p>Description: {description}</p>
         </Box>
+      </Flex>
+      <Heading mt={8} size='lg'>
+        Event Stalls
+      </Heading>
+      <Flex mb={10} w='60%' mt={10} direction='column'>
+        <TableContainer>
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th>Type</Th>
+                <Th>Price</Th>
+                <Th isNumeric>Available</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Food Stall</Td>
+                <Td>P500.00</Td>
+                <Td isNumeric>10</Td>
+                <Td>
+                  <Button>Request Stall</Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Food Stall</Td>
+                <Td>P500.00</Td>
+                <Td isNumeric>10</Td>
+                <Td>
+                  <Button>Request Stall</Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Flex>
     </Flex>
-    <Heading mt={8} size='lg'>Event Stalls</Heading>
-    <Flex mb={10} w="60%" mt={10} direction="column">
-    <TableContainer>
-  <Table variant='simple'>
-    <Thead>
-      <Tr>
-        <Th>Type</Th>
-        <Th>Price</Th>
-        <Th isNumeric>Available</Th>
-        <Th></Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>Food Stall</Td>
-        <Td>P500.00</Td>
-        <Td isNumeric>10</Td>
-        <Td><Button>Request Stall</Button></Td>
-      </Tr>
-      <Tr>
-        <Td>Food Stall</Td>
-        <Td>P500.00</Td>
-        <Td isNumeric>10</Td>
-        <Td><Button>Request Stall</Button></Td>
-      </Tr>
-    </Tbody>
-    
-  </Table>
-</TableContainer>
-    </Flex>
-    </Flex>
-  )
-}
+  );
+};
 
-export default SingleEvent
+export default SingleEvent;
--- pages/vendor/events/[eventId].jsx
+++ pages/vendor/events/[eventId].jsx
@@ -1,7 +1,22 @@
-import React from "react";
-import SingleEvent from "../../../components/Shared/SingleEvent"
+import serverApi from '../../../utils/serverApi';
+import React from 'react';
+import SingleEvent from '../../../components/Shared/SingleEvent';
 const SingleEventPage = () => {
-  return <SingleEvent />;
+  const [event, setEvent] = React.useState();
+
+  const getEvent = async (id) => {
+    const {
+      data: { event },
+    } = await serverApi.get(`http://localhost:3000/api/v1/events/${id}`);
+    console.log(event);
+    setEvent(event);
+  };
+
+  React.useEffect(() => {
+    getEvent('6273a6494713c03138fa7ab5');
+  }, []);
+
+  return <SingleEvent event={event} />;
 };
 
 export default SingleEventPage;
