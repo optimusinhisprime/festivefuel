bd5fb27544718d04721225fef5d4a7c9871ec5f9
Les Chinyanga <les.chinyanga@africacode.academy>
2022-05-06 18:54:32 +0200

    Reinstate SessionProvider import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout/Layout';
 import { AppProvider } from '../context/appContext';
+import { SessionProvider } from 'next-auth/react';
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
