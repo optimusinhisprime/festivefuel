872bc5d557d460c53678bc3343cb6166d56dea67
scarface <pphiri1012x@gmail.com>
2022-05-06 15:44:28 +0200

    Create organizer dashboard

--- pages/organizer/dashboard.jsx
+++ pages/organizer/dashboard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Flex, Heading } from '@chakra-ui/react'
+import Event from "../../components/Event/Event"
+import StallRequest from "../../components/Event/StallRequest";
 
 const OrganizerDashboard = () => {
-  return <div>Organizer Dashboard</div>;
+  return <Flex h="100vh" direction="column" align="center">
+    <Heading mt={10} as='h3' size='lg'>
+    My Events
+  </Heading>
+  <Flex align="center" w="50%" direction="column" mt={8}>
+    <Event/>
+    
+  </Flex>
+  <Heading mt={10} as='h3' size='lg'>
+    Stall Requests
+  </Heading>
+  <Flex w="50%" direction="column" mt= {8}>
+  <StallRequest/>
+  </Flex>
+  </Flex>;
 };
 
 export default OrganizerDashboard;
