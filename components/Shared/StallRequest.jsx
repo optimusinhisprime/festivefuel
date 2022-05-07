fba676568fce679588c1246d4861b0c08d7870a5
Les Chinyanga <les.chinyanga@africacode.academy>
2022-05-07 14:31:49 +0200

    Add more StallRequest settings

--- components/Shared/StallRequest.jsx
+++ components/Shared/StallRequest.jsx
@@ -1,12 +1,20 @@
 import { Badge, Text, Box, Flex, Divider } from '@chakra-ui/react';
+import moment from 'moment';
 import React from 'react';
 
 const StallRequest = ({ stall }) => {
-  const { requestStatus, event, createdAt } = stall;
+  const { requestStatus, event, createdAt, expirationDate } = stall;
   return (
     <Box p={2} borderRadius='sm'>
+      <Text>Event: {event?.name}</Text>
       <Text>Request ID: {stall._id}</Text>
-      <Text>Date Created: {createdAt}</Text>
+      <Text>
+        Date Created: {moment(createdAt).format('MMMM Do YYYY, h:mm:ss a')}
+      </Text>
+      Expires in:
+      {requestStatus === 'pending' && (
+        <Text>{moment().endOf(expirationDate).fromNow()}</Text>
+      )}
       <Flex>
         <Box>
           <Text fontWeight='bold' fontSize='lg'>
