5f326b3e346139e60c24b37f79f315436a528d1b
Les Chinyanga <les.chinyanga@africacode.academy>
2022-05-07 14:19:27 +0200

    Initial stuff for StallRequest

--- components/Shared/StallRequest.jsx
+++ components/Shared/StallRequest.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 
 const StallRequest = ({ stall }) => {
   const { requestStatus, event, createdAt } = stall;
-  return <Text>Stall Request</Text>;
+  return (
+    <Box p={2} borderRadius='sm'>
+      <Text>Request ID: {stall._id}</Text>
+      <Text>Date Created: {createdAt}</Text>
+      <Flex>
+        <Box>
+          <Text fontWeight='bold' fontSize='lg'>
+            Status:
+          </Text>
+          <Badge
+            ml='1'
+            colorScheme={
+              requestStatus === 'pending'
+                ? 'gray'
+                : requestStatus === 'approved'
+                ? 'green'
+                : 'red'
+            }
+          >
+            {requestStatus}
+          </Badge>
+        </Box>
+      </Flex>
+      <Divider />
+    </Box>
+  );
 };
 
 export default StallRequest;
