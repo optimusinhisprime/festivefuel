cb88918e66c1039f8dec9afc7e00f1eb82d41e23
scarface <pphiri1012x@gmail.com>
2022-05-09 17:41:09 +0200

    Make navbar responsive

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -1,30 +1,47 @@
-import React,{ useEffect, useState } from 'react'
-import { Flex, Box, Link, Image, Button, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
-import { GrLogin, GrFormDown } from 'react-icons/gr';
+import React, { useEffect, useState } from "react";
+import {
+  Flex,
+  Box,
+  Link,
+  Image,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { GrLogin, GrFormDown } from "react-icons/gr";
 
-export default function Navbar({children}) {
+export default function Navbar({ children }) {
   return (
-    <Flex bg="#000E44" pr={7} pl={7}  align="center" justify="space-between">
-        <Box>
-            <Image w={200} src="/festivalfuel-logo.png"/>
-        </Box>
+    <Flex
+      direction={["column", "row"]}
+      bg="#000E44"
+      pr={7}
+      pl={7}
+      align="center"
+      justify="space-between"
+    >
+      <Box>
+        <Image w={["100%", 200]} src="/festivalfuel-logo.png" />
+      </Box>
 
-        <Flex >
+      <Flex mt={[10, 0, 0, 0]} mb={[5, 0, 0, 0]}>
         <Menu>
-  <MenuButton size="sm" as={Button} rightIcon={<GrFormDown />}>
-    Profile
-  </MenuButton>
-  <MenuList>
-    
-    <MenuItem>Stall Requests</MenuItem>
-    <MenuItem>Shopping Cart</MenuItem>
-  <MenuItem>Account Settings</MenuItem>
-   <MenuItem>Logout</MenuItem>
-  </MenuList>
-</Menu>
-          <Button size="sm" ml={3} leftIcon={<GrLogin/>} variant="solid">Sign In</Button>
-          </Flex>
-      
+          <MenuButton size="sm" as={Button} rightIcon={<GrFormDown />}>
+            Profile
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Stall Requests</MenuItem>
+            <MenuItem>Shopping Cart</MenuItem>
+            <MenuItem>Account Settings</MenuItem>
+            <MenuItem>Logout</MenuItem>
+          </MenuList>
+        </Menu>
+        <Button size="sm" ml={3} leftIcon={<GrLogin />} variant="solid">
+          Sign In
+        </Button>
+      </Flex>
     </Flex>
-  )
+  );
 }
