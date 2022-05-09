59bde2bb9a4c8dcc9a570b4816ef09a794ceaac9
scarface <pphiri1012x@gmail.com>
2022-05-09 18:53:44 +0200

    Add bodyparser

--- pages/api/v1/events/index.js
+++ pages/api/v1/events/index.js
@@ -2,15 +2,18 @@ import { databaseConnection } from "../../../../utils/db";
 import Cors from "cors";
 import { runMiddleware } from "../../../../middleware/initMiddleware";
 import { allEventsHandler } from "../../../../handlers/allEventsHandler";
+import formidable from "formidable";
 
 // Initializing the cors middleware
 const cors = Cors({
   methods: ["GET", "POST"],
 });
 
+export { allEventsHandler as allEventsHandler };
 export const config = {
   api: {
     externalResolver: true,
+    bodyParser: false,
   },
 };
 
