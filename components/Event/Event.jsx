f8d2a078100a64c3eaa2811751e607a5551a3fd0
scarface <pphiri1012x@gmail.com>
2022-05-09 17:43:24 +0200

    Add alert dialog components

--- components/Event/Event.jsx
+++ components/Event/Event.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import { Link ,Flex, Text, VStack, StackDivider, Image} from '@chakra-ui/react'
+import React from "react";
+import {
+  Link,
+  Flex,
+  Text,
+  VStack,
+  StackDivider,
+  Image,
+} from "@chakra-ui/react";
 
-const Event = ({event}) => {
-  const { name, date, venue, images } = event;
+const Event = ({ event }) => {
+  const { eventName, date, venue, images } = event;
   return (
-    <Flex direction={["column","column","row","row"]} mt={2} mb={2} p={2} border='2px' borderColor='gray.400' w="100%" cursor="pointer" >
-      <Image w={["100%", "100%", "50%",  "30%"]} h="auto" src={images[0]}/>
-      <VStack p={5} w="100%" divider={<StackDivider borderColor='gray.400' />}>
-        <Text>{name}</Text>
+    <Flex
+      direction={["column", "column", "row", "row"]}
+      mt={2}
+      mb={2}
+      p={2}
+      border="2px"
+      borderColor="gray.400"
+      w="100%"
+      cursor="pointer"
+    >
+      <Image w={["100%", "100%", "50%", "30%"]} h="auto" src={images[0]} />
+      <VStack p={5} w="100%" divider={<StackDivider borderColor="gray.400" />}>
+        <Text>{eventName}</Text>
         <Text>{date}</Text>
         <Text>{venue}</Text>
-        <Link color='orange'>View More Information</Link>
+        <Link color="orange">View More Information</Link>
       </VStack>
     </Flex>
-  )
-}
+  );
+};
 
-export default Event
+export default Event;
